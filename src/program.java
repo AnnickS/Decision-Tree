6743358071094eb6f41b52730ba8c5192cf7601a
AnnickS <32581380+AnnickS@users.noreply.github.com>
2018-10-24 19:55:59 -0700

    Check 100 time

--- src/program.java
+++ src/program.java
@@ -4,11 +4,12 @@ public class program {
 	static decisionTree loanTree;
 
 	public static void main(String[] args) {
+		for(int i = 0; i <= 100; i++) {
 		loanTree = new decisionTree(new File("data/credit.csv"));
 		loanTree.createTestData(.1);
 		loanTree.buildDecisionTree();
-		loanTree.listAttributeTitles();
 		loanTree.testTree();
+		}
 	}
 
 }
